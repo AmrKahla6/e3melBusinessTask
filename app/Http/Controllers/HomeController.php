7469a48fd884d25f4753441f087bf34f974b8942
AmrKahla6 <amrkahla6@gmail.com>
2022-01-11 21:39:21 +0200

    Category & Rate filtter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Course;
+use App\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,13 +11,17 @@ class HomeController extends Controller
     /**
      * Show all courses detials in home page
      */
-    public function index(Request $request){
-        $courses =  Course::whereHas('category', function ($query) {
-            //If active = 0 show category else hide
-            return $query->where('active', '=', 0);
-        })->where('active',0)->paginate(9);
+    public function index(Request $request)
+    {
+        $courses =  Course::with('category')
+            ->whereHas('category', function ($query) {
+                $query->where('active', '=', 0);
+            })
+            ->where('active', 0)
+            ->paginate(9);
 
-        return view('welcome',compact('courses'));
+        $categories = Category::where('active', 0)->get();
+        return view('welcome', compact('courses', 'categories'));
     }
 
     /**
@@ -24,14 +29,19 @@ class HomeController extends Controller
      */
     function fetchData(Request $request)
     {
-        if($request->ajax())
-        {
-            $courses =  Course::whereHas('category', function ($query) {
-                //If active = 0 show category else hide
-                return $query->where('active', '=', 0);
-            })->where('active',0)->paginate(9);
+        $courses =  Course::whereHas('category', function ($query) {
+            $query->where('active', '=', 0);
+        })
+        ->when($request->category_id && $request->category_id != null, function($q) use($request){
+            $q->whereHas('category', function ($q2) use($request) {
+                $q2->where('id', $request->category_id);
+            });
+        })
+        ->when($request->rate_id && $request->rate_id != null, function($q) use($request){
+            $q->where('rating', $request->rate_id);
+        })
+        ->where('active',0)->paginate(1);
 
-            return view('welcome_paginate',compact('courses'))->render();
-        }
+        return view('welcome_paginate',compact('courses'))->render();
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,61 +1,88 @@
 
 @section('styles')
-    <style>
-        .checked {
-            color: orange;
-        }
-    </style>
+<style>
+    .checked {
+        color: orange;
+    }
+</style>
 @show
 @extends('layout')
 @section('content')
-{{-- <div class="container">
-    <div class="row">
-      <div class="col">
-          <select name="" id="category_id" class="form-control mt-2">
-                <option value="" disabled selected>Choose Category</option>
-          </select>
-      </div>
-      <div class="col">
-        <select name="" id="" class="form-control mt-2">
-            <option value="" disabled selected>Choose Rate</option>
-        </select>
-      </div>
-      <div class="col">
-        <select name="" id="" class="form-control mt-2">
-            <option value="" disabled selected>Choose Levels</option>
-        </select>
-      </div>
-    </div>
-  </div> --}}
-<div class="row row-cols-1 row-cols-md-3 g-4 m-4" id="card_data">
-    @if (count($courses) != 0)
-        @include('welcome_paginate')
-    @endif
+<div class="container">
+<div class="row">
+  <div class="col">
+      <select name="" id="category_id" class="form-control mt-2">
+            <option value="" disabled selected>Choose Category</option>
+            @foreach($categories as $category)
+              <option value="{{$category->id}}">{{$category->name}}</option>
+            @endforeach
+      </select>
   </div>
+  <div class="col">
+    <select name="" id="rate_id" class="form-control mt-2">
+        <option value="" disabled selected>Choose Rate</option>
+        @for ($i = 5; $i >= 1; $i--)
+          <option value="{{$i}}">{{$i}}</option>
+        @endfor
+
+    </select>
+  </div>
+  <div class="col">
+    <select name="" id="" class="form-control mt-2">
+        <option value="" disabled selected>Choose Levels</option>
+    </select>
+  </div>
+</div>
+</div>
+<input type="hidden" value="" id="stack_category_id">
+<input type="hidden" value="" id="stack_rate_id">
+<div class="row row-cols-1 row-cols-md-3 g-4 m-4" id="card_data">
+@if (count($courses) != 0)
+    @include('welcome_paginate')
+@endif
+</div>
 @endsection
 
 
 @section('scripts')
-  <script>
-      //Ajax code to paginate without reload page
-      $(document).ready(function(){
-        $(document).on('click', '.pagination a', function(event){
-            event.preventDefault();
-            var page = $(this).attr('href').split('page=')[1];
-            fetch_data(page);
-        });
+<script>
+$(".container").on("change", "#category_id", function(e){
+  e.preventDefault()
+  var product     = $("option:selected", this);
+  var category_id = this.value;
+  $('#stack_category_id').val(category_id);
+  let rate_id = $('#stack_rate_id').val();
+  fetch_data(1, category_id, rate_id);
+});
 
-        //Function to fetch date using ajax
-        function fetch_data(page){
-            $.ajax({
-                url:"fetch_data?page="+page,
-                success:function(data)
-                {
-                    $('#card_data').html(data);
-                }
-            });
-        }
-      });
-  </script>
+$(".container").on("change", "#rate_id", function(e){
+  e.preventDefault()
+  var product     = $("option:selected", this);
+  var rate_id     = this.value;
+  $('#stack_rate_id').val(rate_id);
+  let category_id = $('#stack_category_id').val();
+  fetch_data(1, category_id, rate_id);
+});
+
+$(document).on('click', '.pagination a', function(event){
+    event.preventDefault();
+    var page = $(this).attr('href').split('page=')[1];
+    let category_id = $('#stack_category_id').val();
+    let rate_id = $('#stack_rate_id').val();
+    var page = $(this).attr('href').split('page=')[1];
+    fetch_data(page, category_id, rate_id);
+});
+
+function fetch_data(page, category_id, rate_id){
+  $.ajax({
+      url:"fetch_data?page="+page,
+      data: {category_id: category_id, rate_id: rate_id},
+      success:function(data)
+      {
+          $('#card_data').html(data);
+      }
+  });
+}
+</script>
 @endsection
 
